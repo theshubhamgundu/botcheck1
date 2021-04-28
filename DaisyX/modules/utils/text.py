905f9030a19bb220a9c5162a7140e5b4fec07161
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-28 18:27:26 +0530

    Update text.py

--- DaisyX/modules/utils/text.py
+++ DaisyX/modules/utils/text.py
@@ -1,4 +1,4 @@
-# This file is part of Daisy (Telegram Bot)
+# This file is part of EMAA BOT
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# This file is part of Daisy.
+# This file is part of EMAA BOT.
 
 from typing import Union
 
