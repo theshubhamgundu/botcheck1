8c808345a8a0c805aa05a820368018f32133ef5a
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-28 18:26:11 +0530

    Update restrictions.py

--- DaisyX/modules/utils/restrictions.py
+++ DaisyX/modules/utils/restrictions.py
@@ -1,4 +1,4 @@
-# This file is part of Daisy (Telegram Bot)
+# This file is part of EMAA BOT
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -16,7 +16,7 @@
 from aiogram.types.chat_permissions import ChatPermissions
 from aiogram.utils.exceptions import BadRequest, MigrateToChat, Unauthorized
 
-from DaisyX import bot
+from EMAA import bot
 
 
 async def ban_user(chat_id, user_id, until_date=None):
