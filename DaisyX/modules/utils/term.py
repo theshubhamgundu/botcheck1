e1e13670bb10fb963159d377dec54d575e3ef46d
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-28 18:26:46 +0530

    Update term.py

--- DaisyX/modules/utils/term.py
+++ DaisyX/modules/utils/term.py
@@ -1,4 +1,4 @@
-# This file is part of Daisy (Telegram Bot)
+# This file is part of EMAA BOT
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -16,7 +16,7 @@
 import asyncio
 import subprocess
 
-from DaisyX.services.telethon import tbot
+from EMAA.services.telethon import tbot
 
 
 async def chat_term(message, command):
